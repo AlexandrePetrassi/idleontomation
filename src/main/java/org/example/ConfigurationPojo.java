4e0ec7971697bd89653b865d8a45120fb2ecb7f1
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-10 22:51:34 -0300

    Add: ConfigurationPojo

---
+++ src/main/java/org/example/ConfigurationPojo.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class ConfigurationPojo {
+    private AutoIt autoIt;
+
+    public AutoIt getAutoIt() {
+        return autoIt;
+    }
+    public static class AutoIt {
+        private Dll dll;
+
+        public Dll getDll() {
+            return dll;
+        }
+
+        public static class Dll {
+            private String name;
+            private String directory;
+
+            public String getName() {
+                return name;
+            }
+
+            public String getDirectory() {
+                return directory;
+            }
+        }
+    }
+}
