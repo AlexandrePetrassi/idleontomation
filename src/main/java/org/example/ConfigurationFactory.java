49dbcaa8c86af25ba3e017d1d6379c17aefda3f5
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-10 22:51:43 -0300

    Add: ConfigurationFactory

---
+++ src/main/java/org/example/ConfigurationFactory.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class ConfigurationFactory {
+
+    private static final String DEFAULT_CONFIG_NAME = "config.yaml";
+
+    private static ConfigurationPojo cache;
+
+    private ConfigurationFactory() {
+        throw new IllegalStateException("Utility Class");
+    }
+
+    public static ConfigurationPojo create() {
+        if (cache == null) {
+            cache = YamlLoader.load(ConfigurationPojo.class, DEFAULT_CONFIG_NAME);
+        }
+        return cache;
+    }
+}
