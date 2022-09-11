9481fa5e2feaed343c766f41517276e116a6dcfa
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-10 22:53:01 -0300

    Refactor: main method now opens the game

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,29 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import autoitx4java.AutoItX;
+
+import java.awt.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void focusIdleOnWindow(AutoItX autoItX, String windowName) {
+        autoItX.winActivate(windowName);
+        autoItX.winWaitActive(windowName, "", 1);
+    }
+
+    public static Rectangle getIdleOnScreenRect(AutoItX autoItX, String windowName) {
+        return new Rectangle(
+                autoItX.winGetPosX(windowName),
+                autoItX.winGetPosY(windowName),
+                autoItX.winGetPosWidth(windowName),
+                autoItX.winGetPosHeight(windowName)
+        );
+    }
+
+    public static void main(String[] args) {
+        AutoItX autoItX = AutoItXFactory.create();
+        Paint.drawCircleUsingPaint(autoItX);
+        focusIdleOnWindow(autoItX, "Legends Of Idleon");
     }
 }
