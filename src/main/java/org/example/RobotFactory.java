837c536ad8f38d7b4172ff9f168189e606cfbaa9
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-10 23:07:02 -0300

    Add: RobotFactory

---
+++ src/main/java/org/example/RobotFactory.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.awt.*;
+
+public class RobotFactory {
+
+    public static final String ROBOT_INSTANTIATION_ERROR =
+            "Problems while instantiate robot";
+
+    private static Robot cachedRobot;
+
+    private RobotFactory() {
+        throw new IllegalStateException("Utility Class");
+    }
+
+    private static Robot createRobot() {
+        try {
+            return new Robot();
+        } catch (AWTException e) {
+            throw new IllegalStateException(ROBOT_INSTANTIATION_ERROR, e);
+        }
+    }
+
+    public static Robot create() {
+        if (cachedRobot == null) {
+            cachedRobot = createRobot();
+        }
+        return cachedRobot;
+    }
+}
