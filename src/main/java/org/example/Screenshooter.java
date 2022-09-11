d669532df68e339e872babcc9732ea019f669040
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-10 23:07:14 -0300

    Add: Screenshooter

---
+++ src/main/java/org/example/Screenshooter.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Screenshooter {
+
+    private Screenshooter() {
+        throw new IllegalStateException("Utility Class");
+    }
+
+    public static BufferedImage screenshot(Rectangle screenshotArea) {
+        return RobotFactory.create().createScreenCapture(screenshotArea);
+    }
+
+
+}
