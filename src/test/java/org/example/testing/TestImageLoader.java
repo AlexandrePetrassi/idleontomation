17c6aa12d70c284101aa8ee5d9e76dadb8e89836
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-12 13:23:52 -0300

    Test: add TestImageLoader

---
+++ src/test/java/org/example/testing/TestImageLoader.java
@@ -0,0 +1,33 @@
+package org.example.testing;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+public class TestImageLoader {
+
+    private static final String FAIL_TO_READ_IMAGE =
+            "Failed to read image from file \"%s\"";
+
+    private static final String FILE_NOT_FOUND =
+            "File \"%s\" not found";
+
+    private TestImageLoader() {
+        throw new IllegalStateException("Utility Class");
+    }
+
+    private static URL getUrl(String filename) {
+        URL url = TestImageLoader.class.getResource(filename);
+        if (url == null) throw new IllegalStateException(String.format(FILE_NOT_FOUND, filename));
+        return url;
+    }
+
+    public static BufferedImage load(String filename) {
+        try {
+            return ImageIO.read(getUrl(filename));
+        } catch (IOException e) {
+            throw new IllegalStateException(String.format(FAIL_TO_READ_IMAGE, filename), e);
+        }
+    }
+}
