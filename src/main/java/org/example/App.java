6352734ba91836d39e541878f2f6aa7a375ca0e7
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-11 21:40:41 -0300

    Refactor: App#main to start the chop mini-game

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,29 +1,47 @@
 package org.example;
 
 import autoitx4java.AutoItX;
+import org.example.automation.AutoItXFactory;
+import org.example.automation.Keyboard;
+import org.example.graphics.ImageLoader;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
-public class App 
-{
+import static org.example.automation.AutoItXExtensions.getGameArea;
+import static org.example.idleon.ChopMiniGame.keepClicking;
 
-    public static void focusIdleOnWindow(AutoItX autoItX, String windowName) {
-        autoItX.winActivate(windowName);
-        autoItX.winWaitActive(windowName, "", 1);
+public class App {
+
+    public static Rectangle gameArea(AutoItX autoItX) {
+        String windowName = "Legends Of Idleon";
+        BufferedImage chop = ImageLoader.load("/chop.bmp");
+        Rectangle area = new Rectangle(-235, -20, 0, 35);
+        return getGameArea(autoItX, windowName, chop, area);
     }
 
-    public static Rectangle getIdleOnScreenRect(AutoItX autoItX, String windowName) {
-        return new Rectangle(
-                autoItX.winGetPosX(windowName),
-                autoItX.winGetPosY(windowName),
-                autoItX.winGetPosWidth(windowName),
-                autoItX.winGetPosHeight(windowName)
-        );
+    public static void startChopMiniGame(AutoItX autoItX, Rectangle gameArea) {
+        BufferedImage leaf = ImageLoader.load("/leaf.bmp");
+        Rectangle offset = new Rectangle(0, 10, 1, 35);
+        keepClicking(autoItX, leaf, gameArea, offset);
+    }
+
+    public static void automate() {
+        AutoItX autoItX = AutoItXFactory.create();
+        Rectangle gameArea = gameArea(autoItX);
+        if (gameArea == null) {
+            System.out.println("Chop mini game not found");
+            return;
+        }
+        startChopMiniGame(autoItX, gameArea);
     }
 
     public static void main(String[] args) {
-        AutoItX autoItX = AutoItXFactory.create();
-        Paint.drawCircleUsingPaint(autoItX);
-        focusIdleOnWindow(autoItX, "Legends Of Idleon");
+        Keyboard.start();
+        try {
+            automate();
+        } finally {
+            Keyboard.stop();
+        }
     }
 }
