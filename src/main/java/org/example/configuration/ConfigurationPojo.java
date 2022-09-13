599f1f27b0622836474f848d87dedb955d01692c
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-13 01:02:12 -0300

    Add: ChopMiniGamePojo

--- src/main/java/org/example/configuration/ConfigurationPojo.java
+++ src/main/java/org/example/configuration/ConfigurationPojo.java
@@ -1,12 +1,19 @@
 package org.example.configuration;
 
 import org.example.automation.AutoItXPojo;
+import org.example.idleon.ChopMiniGamePojo;
 
 public class ConfigurationPojo {
+
     private AutoItXPojo autoItX;
+    private ChopMiniGamePojo chopMiniGame;
 
     public AutoItXPojo getAutoItX() {
         return autoItX;
+    }
+
+    public ChopMiniGamePojo getChopMiniGame() {
+        return chopMiniGame;
     }
 
 }
---
+++ src/main/java/org/example/idleon/ChopMiniGamePojo.java
@@ -0,0 +1,61 @@
+package org.example.idleon;
+
+import java.util.List;
+
+public class ChopMiniGamePojo {
+
+    private String appName;
+    private String frameReference;
+    private String cursorReference;
+    private int forceExitKey;
+    private Messages messages;
+    private List<String> colors;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public String getFrameReference() {
+        return frameReference;
+    }
+
+    public String getCursorReference() {
+        return cursorReference;
+    }
+
+    public int getForceExitKey() {
+        return forceExitKey;
+    }
+
+    public Messages getMessages() {
+        return messages;
+    }
+
+    public List<String> getColors() {
+        return colors;
+    }
+
+    public static class Messages {
+
+        private String frameNotFound;
+        private String cursorNotFound;
+        private String forceExit;
+        private String onClick;
+
+        public String getFrameNotFound() {
+            return frameNotFound;
+        }
+
+        public String getCursorNotFound() {
+            return cursorNotFound;
+        }
+
+        public String getForceExit() {
+            return forceExit;
+        }
+
+        public String getOnClick() {
+            return onClick;
+        }
+    }
+}
