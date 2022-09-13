00ac83812efca8f08ae8023a546477c95f9b3979
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-13 15:05:37 -0300

    Add: Messages class for localization

---
+++ src/main/java/com/caracrazy/localization/Messages.java
@@ -0,0 +1,49 @@
+package com.caracrazy.localization;
+
+public class Messages {
+
+    public static Messages messages() {
+        return MessagesFactory.getMessages();
+    }
+
+    private String errorUtilityClass = "em0";
+    private String errorNativeHookUnregister = "em1";
+    private String errorNativeHookRegister = "em2";
+    private String errorRobotInstantiation = "em3";
+    private String errorFrameNotFound = "em4";
+    private String errorCursorNotFound = "em5";
+    private String infoForceExit = "im1";
+    private String infoClick = "im2";
+
+    public String getErrorUtilityClass() {
+        return errorUtilityClass;
+    }
+
+    public String getErrorNativeHookUnregister() {
+        return errorNativeHookUnregister;
+    }
+
+    public String getErrorNativeHookRegister() {
+        return errorNativeHookRegister;
+    }
+
+    public String getErrorRobotInstantiation() {
+        return errorRobotInstantiation;
+    }
+
+    public String getErrorFrameNotFound() {
+        return errorFrameNotFound;
+    }
+
+    public String getErrorCursorNotFound() {
+        return errorCursorNotFound;
+    }
+
+    public String getInfoForceExit() {
+        return infoForceExit;
+    }
+
+    public String getInfoClick() {
+        return infoClick;
+    }
+}
---
+++ src/main/java/com/caracrazy/localization/MessagesFactory.java
@@ -0,0 +1,31 @@
+package com.caracrazy.localization;
+
+import com.caracrazy.yaml.YamlLoader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MessagesFactory {
+
+    private MessagesFactory() {
+        throw new IllegalStateException("Utility Class");
+    }
+
+    private static final String LOCALE_KEY = "user.language";
+    private static final String MESSAGES_FILE_PATTERN = "messages-%s.yaml";
+
+    private static final Map<String, Messages> cache = new HashMap<>();
+
+    public static Messages getMessages() {
+        return cache.computeIfAbsent(System.getProperty(LOCALE_KEY), MessagesFactory::load);
+    }
+
+    private static Messages load(String locale) {
+        try {
+            String filename = String.format(MESSAGES_FILE_PATTERN, locale);
+            return YamlLoader.load(Messages.class, filename);
+        } catch (IllegalStateException e) {
+            return new Messages();
+        }
+    }
+}
