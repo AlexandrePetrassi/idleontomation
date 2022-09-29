6257bddbcb72fa9fb123ac027f8f340807f71657
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-09-29 20:19:47 -0300

    Refactor: replaced ternary for if clause in loadConfig

--- src/main/java/com/caracrazy/App.java
+++ src/main/java/com/caracrazy/App.java
@@ -6,13 +6,18 @@ import com.caracrazy.idleon.ChopMiniGameInitializer;
 
 public class App {
 
+    private static final String DEFAULT_CONFIG_FILE = "config.yaml";
+
     public static void main(String[] args) {
         ConfigurationData config = loadConfig(args);
         ChopMiniGameInitializer.initialize(config.getAutoItX(), config.getChopMiniGame());
     }
 
     private static ConfigurationData loadConfig(String[] args) {
-        String configFile = ((args.length > 0) ? args[0] : "config") + ".yaml";
-        return ConfigurationFactory.create(configFile);
+        if (args != null && args.length > 0) {
+            return ConfigurationFactory.create(args[0] + ".yaml");
+        } else {
+            return ConfigurationFactory.create(DEFAULT_CONFIG_FILE);
+        }
     }
 }
