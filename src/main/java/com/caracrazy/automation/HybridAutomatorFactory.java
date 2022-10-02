9155dab8d91359280a106ef96560f1ba153a3247
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-10-02 00:45:00 -0300

    Add: HybridAutomatorFactory

---
+++ src/main/java/com/caracrazy/automation/HybridAutomatorFactory.java
@@ -0,0 +1,24 @@
+package com.caracrazy.automation;
+
+import autoitx4java.AutoItX;
+import com.caracrazy.automation.autoit.AutoItXAutomator;
+import com.caracrazy.automation.autoit.AutoItXData;
+import com.caracrazy.automation.autoit.AutoItXFactory;
+import com.caracrazy.automation.robot.RobotFactory;
+
+import java.awt.*;
+
+import static com.caracrazy.localization.Messages.messages;
+
+public class HybridAutomatorFactory {
+
+    private HybridAutomatorFactory() {
+        throw new IllegalStateException(messages().getErrorUtilityClass());
+    }
+
+    public static Automator create(AutoItXData config) {
+        Robot robot = RobotFactory.create();
+        AutoItX autoItX = AutoItXFactory.INSTANCE.create(config);
+        return new AutoItXAutomator(autoItX, robot);
+    }
+}
