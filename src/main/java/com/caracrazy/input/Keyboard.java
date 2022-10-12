884b15b164bf5987b71b7716b6255ed884469c51
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-10-12 16:58:48 -0300

    Refactor: Keyboard interface

--- src/main/java/com/caracrazy/input/Keyboard.java
+++ src/main/java/com/caracrazy/input/Keyboard.java
@@ -1,5 +1,10 @@
 package com.caracrazy.input;
 
 public interface Keyboard {
+
     boolean isKeyPressed(int keyCode);
+
+    void addKeyEventListener(int keyCode, KeyboardEvent event, Runnable callback);
+
+    void removeKeyEventListener(int keyCode, KeyboardEvent event, Runnable callback);
 }
